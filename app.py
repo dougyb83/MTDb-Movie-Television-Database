32c2211e22cb5bfc09e9fd9f241a8884a31a5ae9
Doug B <dougy.b@proton.me>
2023-08-28 06:27:36 +0000

    Add login functionality

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/home", methods=["GET", "POST"])
 def home():
+    # register new account
     if request.method == "POST" and request.form.get("signup-username"):
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
@@ -39,6 +40,32 @@ def home():
         session["user"] = request.form.get("signup-username").lower()
         flash("Registration Successful!")
         # return redirect(url_for("profile", username=session["user"]))
+
+    # log in
+    if request.method == "POST" and request.form.get("login-username"):
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("login-username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get(
+                            "login-username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("login-username")))
+                        # return redirect(url_for(
+                        #     "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("home"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
     movies = mongo.db.movies.find()
     return render_template("home.html", movies=movies)
 
